e580adcaa8953182604758723cba90872cd8b7c9
Andrew Sanchez <bballbizniz@aol.com>
2017-09-28 21:10:17 -0700

    update callback for local deployment

--- react-ui/src/Auth/auth0-variables.js
+++ react-ui/src/Auth/auth0-variables.js
@@ -2,7 +2,7 @@ export const AUTH_CONFIG = {
   domain: 'keto-app.auth0.com',
   clientId: 'vj2Tz98uE3llbuy3uykxyxy1r3O8yzAD',
   // Local
-  // callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://localhost:3000/callback'
   // Heroku
-  callbackUrl: 'https://kdiet.herokuapp.com/callback'
+  // callbackUrl: 'https://kdiet.herokuapp.com/callback'
 }
